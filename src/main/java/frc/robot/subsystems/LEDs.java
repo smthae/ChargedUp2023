b99c7d24459955d2eb16529de7e02debff5e9af0
pooriaahmadi <pooriababak444@gmail.com>
2023-03-01 11:24:53 -0500

    just trying to not spam pwm

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -6,13 +6,18 @@ import frc.robot.Constants;
 
 public class LEDs extends SubsystemBase {
   private final Spark blinkin = new Spark(Constants.LEDConstants.blinkinPort);
+  private double currentColor = 0;
 
   public LEDs() {
     this.set(Constants.LEDConstants.forestPattern);
   }
 
   public void set(double value) {
+    if (currentColor == value)
+      return;
+
     if (value >= -1.0 && value <= 1.0) {
+      currentColor = value;
       this.blinkin.set(value);
     }
   }
